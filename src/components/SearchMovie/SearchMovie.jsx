8fde9bd0f2c481f7882cfca874d0587dd2445a35
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-06-06 17:27:31 +0300

    filtered

--- src/components/SearchMovie/SearchMovie.jsx
+++ src/components/SearchMovie/SearchMovie.jsx
@@ -1,41 +1,63 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function SearchMovie() {
+  const [typing, setTyping] = useState("");
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+    console.log(movieData);
+  }, [typing]);
+
+  async function getMovies() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const getData = data.results
+      .filter((item) => item.title.toLowerCase().includes(typing.toLowerCase()))
+      .map((i) => {
+        return {
+          id: i.episode_id,
+          title: i.title,
+        };
+      });
+    setMovieData(getData);
+  }
+
+  const typingStart = (e) => {
+    const typingTimeout = setTimeout(() => {
+      setTyping(e.target.value);
+    }, 500);
+
+    return () => {
+      clearTimeout(typingTimeout);
+    };
+  };
+
   return (
     <div>
       <div className="searchBar">
         <div className="mainSearchBar">
           <h2>Search Movie</h2>
-          <input type="text" placeholder="Search..." />
+          <input onChange={typingStart} type="text" placeholder="Search..." />
+          <br />
+          {typing}
         </div>
       </div>
 
       <div className="mainMovie">
         <div className="mainBoxs">
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
-          <div className="boxs">
-              <img src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg" alt="" />
-              <div className="movieInfo">Korkunç Bir Film</div>
-          </div>
+          {movieData.map((item) => {
+            return (
+              <div key={item.id} className="boxs">
+                <img
+                  src="https://flxt.tmsimg.com/assets/p27977_p_v13_af.jpg"
+                  alt=""
+                />
+                <div className="movieInfo">{item.title}</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
